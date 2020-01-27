0da38826aa738ad5027865bd4a0b5284aaf60622
xazw-PC2 <none@none.com>
2020-01-27 16:59:08 +0800

    [EX-100PCPE] Edit EX-001

--- 100-python-challenging-programming-exercises/exercise_001.py
+++ 100-python-challenging-programming-exercises/exercise_001.py
@@ -14,17 +14,3 @@ l = [str(x) for x in range(2000, 3201)
         if (x % 7 == 0) and (x % 5 != 0)]
 print(", ".join(l))
 
-
-"""
----------------------------
-ORIGINAL-SOLUTION:
-
-l=[]
-for i in range(2000, 3201):
-    if (i%7==0) and (i%5!=0):
-        l.append(str(i))
-
-print ','.join(l)
-
-
-"""
