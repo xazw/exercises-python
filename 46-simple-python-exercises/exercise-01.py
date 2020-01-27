f3f7da45100b18322d89a4f0516d513dc5cecdd9
xazw-PC2 <none@none.com>
2020-01-27 16:27:43 +0800

    [EX-46SPE] Add EX-001

---
+++ 46-simple-python-exercises/exercise-01.py
@@ -0,0 +1,17 @@
+# 01
+#
+# Define a function max() that takes two numbers as arguments and returns the largest of them. 
+# Use the if-then-else construct available in Python. (It is true that Python has the max() function built in, but writing it yourself is nevertheless a good exercise.)
+#
+# SOLVED-20190817-191807
+
+
+def new_max(a, b):
+
+    assert b != a, "Values are the same."
+    if a > b:
+        return a
+    elif b > a:
+        return b
+
+print(new_max(46, 46.5))
