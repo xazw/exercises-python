20055e4d3eeeaf4fb1920df2c2aba81d319634a5
xazw-PC2 <none@none.com>
2020-01-27 16:30:10 +0800

    [EX-46SPE] Add EX-002

---
+++ 46-simple-python-exercises/exercise-02.py
@@ -0,0 +1,10 @@
+# 02
+# SOLVED-20190817-191807
+#
+# Define a function max_of_three() that takes three numbers as arguments and returns the largest of them.
+
+
+def max_of_three(a, b, c):
+    return max(a, b, c)
+
+print(max_of_three(1,13,6))
