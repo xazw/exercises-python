70b53167b681c37069fb40889886162191fa58d7
xazw-PC2 <none@none.com>
2020-01-30 17:52:59 +0800

    [EX-46SPE] Add EX-007

---
+++ 46-simple-python-exercises/exercise_007.py
@@ -0,0 +1,11 @@
+"""
+EXERCISE-007
+SOLVED-
+
+- Define a function reverse() that computes the reversal of a string. 
+- E.g. reverse("I am testing") should return the string "gnitset ma I".
+
+
+"""
+
+
