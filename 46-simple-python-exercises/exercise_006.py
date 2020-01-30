e1c8d334c603f254c8fec7efa39bce3ad4e72cc6
xazw-PC2 <none@none.com>
2020-01-30 17:35:52 +0800

    [EX-46SPE] Edit EX-006

--- 46-simple-python-exercises/exercise_006.py
+++ 46-simple-python-exercises/exercise_006.py
@@ -2,7 +2,15 @@
 EXERCISE-006
 SOLVED-
 
+- Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in a list of numbers. 
+- E.g. sum([1, 2, 3, 4]) should return 10, and multiply([1, 2, 3, 4]) should return 24.
 
 
 """
+
+def test+assumptions():
+
+    assert x_sum([1, 2, 3, 4]) == 10
+    assert x_multiply([1, 2, 3, 4]) == 24
+
 
