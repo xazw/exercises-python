63f283157ec763d7d95e7451e43aae8e0f64cc6b
xazw-PC2 <none@none.com>
2020-01-30 17:26:39 +0800

    [EX-46SPE] Edit EX-003

--- 46-simple-python-exercises/exercise_003.py
+++ 46-simple-python-exercises/exercise_003.py
@@ -1,8 +1,19 @@
 """
 EXERCISE-003
-SOLVED-
+SOLVED-20190817-192044
 
-
+Define a function that computes the length of a given list
+or string. (Rewrite Python's len() function.)
 
 """
 
+
+def compute_length(list_item):
+
+    count = 0
+    for i in list_item:
+        count += 1
+    return count
+
+test_list = [1, "apples", 2, "chicken"]
+print( compute_length(test_list) )
