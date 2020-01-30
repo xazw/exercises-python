532cae1c47863f04a7b709b7ce38cbdd0ad31b15
xazw-PC2 <none@none.com>
2020-01-30 17:54:38 +0800

    [EX-46SPE] Edit EX-007

--- 46-simple-python-exercises/exercise_007.py
+++ 46-simple-python-exercises/exercise_007.py
@@ -1,6 +1,6 @@
 """
 EXERCISE-007
-SOLVED-
+SOLVED-20200130-175435
 
 - Define a function reverse() that computes the reversal of a string. 
 - E.g. reverse("I am testing") should return the string "gnitset ma I".
@@ -9,3 +9,8 @@ SOLVED-
 """
 
 
+def reverse(text):
+    return text[::-1]
+
+
+assert reverse("I am testing") == "gnitset ma I"
