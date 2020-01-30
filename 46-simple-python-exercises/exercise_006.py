4adc629714c9a0bb273dcff2971f78f4dc9cdd66
xazw-PC2 <none@none.com>
2020-01-30 17:51:30 +0800

    [EX-46SPE] Edit EX-006

--- 46-simple-python-exercises/exercise_006.py
+++ 46-simple-python-exercises/exercise_006.py
@@ -1,16 +1,37 @@
 """
 EXERCISE-006
-SOLVED-
+SOLVED-20200130-175229
 
-- Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in a list of numbers. 
-- E.g. sum([1, 2, 3, 4]) should return 10, and multiply([1, 2, 3, 4]) should return 24.
+-   Define a function sum() and a function multiply() that
+    sums and multiplies (respectively) all the numbers in a list of numbers.
+-   E.g. sum([1, 2, 3, 4]) should return 10,
+    and multiply([1, 2, 3, 4]) should return 24.
 
 
 """
 
-def test+assumptions():
-
-    assert x_sum([1, 2, 3, 4]) == 10
-    assert x_multiply([1, 2, 3, 4]) == 24
+import operator
 
 
+def x_operation(list_item, op_type):
+    if op_type == 'sum':
+        op_type = 'add'
+        results = 0
+
+    if op_type == 'multiply':
+        op_type = 'mul'
+        results = 1
+
+    for n, i in enumerate(list_item):
+        results = getattr(operator, op_type)(results, i)
+
+    return results
+
+
+def test_assumptions(list_item):
+    assert x_operation(list_item, 'sum') == 10, "Failed to sum"
+    assert x_operation(list_item, 'multiply') == 24, "Failed to multiply"
+
+
+test_list = [1, 2, 3, 4]
+test_assumptions(test_list)
