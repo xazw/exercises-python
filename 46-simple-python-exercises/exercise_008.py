cd49b84b614e29fc449f9a85893edc443cdd7922
xazw-PC2 <none@none.com>
2020-02-02 10:23:24 +0800

    [EX-46SPE] Add EX-008

---
+++ 46-simple-python-exercises/exercise_008.py
@@ -0,0 +1,12 @@
+"""
+EXERCISE-008
+
+
+-   Define a function is_palindrome() that recognizes palindromes 
+    (i.e. words that look the same written backwards). 
+-   For example, is_palindrome("radar") should return True.
+
+
+"""
+
+
